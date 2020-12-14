ff3ea63ea6237646c10edc27284bb700f06c163a
0x01050 <jeraleking@gmail.com>
2020-12-14 18:49:34 +0200

    Bug fix on Request class import

--- app/Http/Controllers/OCRController.php
+++ app/Http/Controllers/OCRController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\File;
-use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Storage;
 use Throwable;
