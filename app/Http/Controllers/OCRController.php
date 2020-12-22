caaa2e1e2687c23109a966244fb9f7e30baffc3a
0x01050 <jeraleking@gmail.com>
2020-12-22 15:34:20 +0200

    Fix bug on Candidate Detect

--- app/Http/Controllers/OCRController.php
+++ app/Http/Controllers/OCRController.php
@@ -370,7 +370,7 @@ class OCRController extends Controller
                                         $page_except = $page['number'];
                                     if($text === 'Borrower' && $page_except !== $page['number'])
                                         $type = 'BORROWER';
-                                    if(!$onlyborrower && $text === 'Co-Borrower' && $page_except !== $page['number'])
+                                    else if(!$onlyborrower && $text === 'Co-Borrower' && $page_except !== $page['number'])
                                         $type = 'COBORROWER';
                                     else
                                         continue;
